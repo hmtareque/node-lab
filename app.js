4f82103f4fa1d3a2f1d8b90f5ddff8ce154846a1
Hasan Tareque <hmtareque@gmail.com>
2020-01-23 18:42:21 +0000

    writing into file

--- app.js
+++ app.js
@@ -7,22 +7,34 @@ const server = http.createServer((req, res) => {
     const url = req.url;
     const method = req.method;
 
-    if(url === '/') {
+
+    if (url === '/') {
 
         res.write('<html>');
         res.write('<head><title>Enter Message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="message" value=""/><button type="submit" name="Send">Send</button></form></body>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message" value=""/><button type="submit">Send</button></form></body>');
         res.write('</html>');
         return res.end();
     }
 
-    if(url === '/message' && method === 'POST') {
+    if (url === '/message' && method === 'POST') {
 
-        fs.writeFileSync('message.txt', 'Hasan');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
 
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
     }
 
     res.setHeader('Content-Type', 'text/html');
