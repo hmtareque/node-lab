47dd08a8a0388f624e34e96819d94d230672eb25
Hasan Tareque <hmtareque@gmail.com>
2020-01-23 18:01:43 +0000

    tested post method

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
+    //console.log(req.url, req.method, req.headers);
+
+    const url = req.url;
+    const method = req.method;
+
+    if(url === '/') {
+
+        res.write('<html>');
+        res.write('<head><title>Enter Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="message" value=""/><button type="submit" name="Send">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+
+    if(url === '/message' && method === 'POST') {
+
+        fs.writeFileSync('message.txt', 'Hasan');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+
+    }
 
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
