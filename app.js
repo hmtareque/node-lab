5ad65f9b17e28baa122baa869b17d3a01e57ac35
Hasan Tareque <hmtareque@gmail.com>
2020-01-23 17:34:29 +0000

    first raw page

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>First Page</title></head>');
+    res.write('<body><h1>Node js is fun</h1></body>');
+    res.write('</html>');
+    res.end();
+
 });
 
 server.listen(3000);
