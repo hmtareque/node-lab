57a5067aebecb6832ca2994a00f0015f2fa804e0
Hasan Tareque <hmtareque@gmail.com>
2020-01-26 20:56:17 +0000

    time view updated

--- timer.js
+++ timer.js
@@ -1,15 +1,23 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 const incTimer = () => {
     currentTime += waitInterval;
-    console.log(`Waiting ${currentTime/1000} seconds`);
+    const percentage = Math.floor((currentTime/waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`... ${percentage}%`);
 }
 
 console.log(`Setting a ${waitTime/1000} for delay`);
 
-const timerFinished = () => console.log('done');
+const timerFinished = () => {
+    clearInterval(interval);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    console.log('done');
+}
 
-setInterval(incTimer, waitInterval);
+const interval = setInterval(incTimer, waitInterval);
 setTimeout(timerFinished, waitTime);
