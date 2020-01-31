ada745f12e4177853ddc93c1bc1066035aa5c259
Hasan Tareque <hmtareque@gmail.com>
2020-01-31 14:03:03 +0000

    list file by exec

--- childprocess.js
+++ childprocess.js
@@ -1,4 +1,12 @@
 const cp = require('child_process');
 
-//cp.exec('open https://tutor.finder.elizaahmed.com');
-cp.exec('open -a Terminal .');
+// cp.exec('open https://tutor.finder.elizaahmed.com');
+// cp.exec('open -a Terminal .');
+
+cp.exec('ls', (error, data) => {
+    if(error){
+        throw error;
+    }
+
+    console.log(data);
+});
